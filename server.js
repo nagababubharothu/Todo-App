38059481d8fa92f9e9638db9b91cda1051c9d821
nagababubharothu <nagababu.23bce20231@vitapstudent.ac.in>
2026-04-16 18:42:17 +0530

    fixed render deployment

--- server.js
+++ server.js
@@ -13,7 +13,6 @@ app.use(express.json());
 // serve static files
 app.use(express.static(path.join(__dirname, "public")));
 
-// ✅ root route → show login page first
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "public", "login.html"));
 });
@@ -23,11 +22,13 @@ app.use("/api/auth", authRoutes);
 const todoRoutes = require("./routes/todoRoutes");
 app.use("/api/todos", todoRoutes);
 
+// MongoDB connection
 mongoose.connect(process.env.MONGO_URI)
 .then(() => console.log("MongoDB Connected"))
 .catch(err => console.log(err));
 
-const PORT = 5000;
+// ✅ FIXED PORT
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
